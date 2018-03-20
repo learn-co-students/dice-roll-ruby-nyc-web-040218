a76a0dacfa04d3d88a19e131172d738ea0df4c09
Ryan O'Toole <otoolerj@gmail.com>
2018-03-20 18:23:57 +0000

    Done.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,15 @@
 # loaded once.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+
+# def roll
+#   rand(1..6)
+# end
+
 RSpec.configure do |config|
+
+
+
   # config here
 end
